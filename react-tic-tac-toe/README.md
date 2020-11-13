acba0564112839463cd60043302ce157b317cd35
Kapica, Marcin <marcin.kapica@sap.com>
2020-11-13 12:53:42 +0100

    react-tic-tac-toe: Initial commit

---
+++ react-tic-tac-toe/README.md
@@ -0,0 +1,14 @@
+# React tic-tac-toe
+
+A React tic-tac-toe game coded by following [Tutorial: Intro to React](https://reactjs.org/tutorial/tutorial.html)
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Installing & running
+
+1. Clone the repository
+2. Go to `react-tic-tac-toe` directory
+3. Run `npm install`
+4. Run `npm start`
+
+App will run in the development mode under [http://localhost:3000](http://localhost:3000).
---
+++ react-tic-tac-toe/src/App.js
@@ -0,0 +1,161 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  renderRow(r) {
+    let squares = [];
+    let rowIncrement = r * this.props.squaresPerRow;
+    for (let s = 0; s < this.props.squaresPerRow; s++) {
+      squares.push(this.renderSquare(s + rowIncrement));
+    }
+    return <div className="board-row">{squares}</div>;
+  }
+
+  render() {
+    const rows = [];
+    for (let r = 0; r < this.props.totalRows; r++) {
+      rows.push(this.renderRow(r));
+    }
+    return <div>{rows}</div>;
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+      isDescendingOrder: false,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
+  reverseStepsDisplayOrder() {
+    this.setState((prevState) => ({
+      isDescendingOrder: !prevState.isDescendingOrder,
+    }));
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const item = this.state.isDescendingOrder
+        ? history.length - 1 - move
+        : move;
+      const desc = item ? 'Go to move #' + item : 'Go to game start';
+      return (
+        <li key={item} className={this.state.stepNumber === item ? 'bold' : ''}>
+          <button onClick={() => this.jumpTo(item)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            totalRows={3}
+            squaresPerRow={3}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+          <button onClick={() => this.reverseStepsDisplayOrder()}>
+            Reverse steps order
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default App;
